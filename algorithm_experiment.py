73da9b60d004a053c7610b16db52015a0a98994e
Ewa <emiklewska@gmail.com>
2025-03-21 17:41:44 +0100

    update

--- algorithm_experiment.py
+++ algorithm_experiment.py
@@ -1,5 +1,4 @@
 import uuid
-
 import mapof.elections as mapof
 import json
 import genetic_algorithm_votes
@@ -7,46 +6,45 @@ import simulated_annealing_votes
 import simulated_annealing_matrix
 import genetic_algorithm_matrix
 import os
+from argparse import ArgumentParser
 
 
-def run(**kwargs):
-    algorithm_name = kwargs['algorithm']
+def run(parameters):
+    algorithm_name = parameters.algorithm
     experiment = get_10x50_experiment()
-
     if algorithm_name in ['simulated_annealing_votes', 'sav']:
         algorithm = lambda: simulated_annealing_votes.anneal(
             experiment=experiment,
-            max_temperature=kwargs.get('max_temperature'),
-            alpha=kwargs.get('alpha'),
-            max_iterations=kwargs.get('max_iterations'),
-            num_voters=kwargs.get('num_voters'),
-            changing_votes=kwargs.get('changing_votes'),
-            neighbor_strategy=kwargs.get('neighbor_strategy'),
-            cooling_schedule=kwargs.get('cooling_schedule')
+            max_temperature=parameters.max_temperature,
+            alpha=parameters.alpha,
+            max_iterations=parameters.max_iterations,
+            num_voters=parameters.num_voters,
+            changing_votes=parameters.changing_votes,
+            neighbor_strategy=parameters.neighbor_strategy,
+            cooling_schedule=parameters.cooling_schedule
         )
     elif algorithm_name in ['simulated_annealing_matrix', 'sam']:
         algorithm = lambda: simulated_annealing_matrix.anneal(
             experiment=experiment,
-            max_temperature=kwargs.get('max_temperature'),
-            alpha=kwargs.get('alpha'),
-            max_iterations=kwargs.get('max_iterations'),
-            neighbor_weight=kwargs.get('neighbor_weight', 1),
-            neighbor_strategy=kwargs.get('neighbor_strategy'),
-            cooling_schedule=kwargs.get('cooling_schedule')
+            max_temperature=parameters.max_temperature,
+            alpha=parameters.alpha,
+            max_iterations=parameters.max_iterations,
+            neighbor_weight=parameters.neighbor_weight,
+            neighbor_strategy=parameters.neighbor_strategy,
+            cooling_schedule=parameters.cooling_schedule
         )
     elif algorithm_name in ['genetic_algorithm_votes', 'genv']:
         algorithm = lambda: genetic_algorithm_votes.genetic_algorithm(
             experiment=experiment,
-            population_size=kwargs.get('population_size'),
-            max_generations=kwargs.get('max_generations'),
-            num_voters=kwargs.get('num_voters', None)
-        )
+            population_size=parameters.population_size,
+            max_generations=parameters.max_iterations,
+            num_voters=parameters.num_vo)
     elif algorithm_name in ['genetic_algorithm_matrix', 'genm']:
         algorithm = lambda: genetic_algorithm_matrix.genetic_algorithm(
             experiment=experiment,
-            population_size=kwargs.get('population_size'),
-            max_generations=kwargs.get('max_generations'),
-            num_voters=kwargs.get('num_voters', 20)
+            population_size=parameters.population_size,
+            max_generations=parameters.max_iterations,
+            num_voters=parameters.num_voters
         )
     else:
         raise ValueError("""Algorithm type not supported. Try:
@@ -54,26 +52,25 @@ def run(**kwargs):
                          simulated_annealing_matrix sam
                          genetic_algorithm_votes genv
                          genetic_algorithm_matrix genm""")
-
-    name = kwargs['name']
-    repetitions = kwargs['reps']
+    name = parameters.name
+    repetitions = parameters.reps
     path = './results/' + name
+    print(path)
     computed_count = 0
 
     if not os.path.exists(path):
         os.makedirs(path, exist_ok=True)
-        save_parameters(kwargs, path)
+        save_parameters(parameters, path)
     else:
         computed_count = sum(1 for i in os.listdir(path) if i[:7] == 'scores_')
 
-    for _ in range(repetitions - computed_count):
-        result = algorithm()
-        result.save_partial(path + "/scores_" + str(uuid.uuid4()) + '.csv')
+    for i in range(repetitions - computed_count):
+        algorithm().save_partial(path + "/scores_" + str(uuid.uuid4()) + '.csv')
 
 
 def save_parameters(parameters, path):
     with open(path + '/parameters.json', 'w') as json_file:
-        json.dump(parameters, json_file, indent=4)
+        json.dump(vars(parameters), json_file, indent=4)
 
 
 def get_10x50_experiment():
@@ -88,12 +85,32 @@ def get_10x50_experiment():
     )
 
 
+def parse_arguments():
+    parser = ArgumentParser()
+    parser.add_argument("name", type=str,
+                        help=""""Name of destination folder""")
+    parser.add_argument("reps", type=int,
+                        help=""""Number of experiments to run""")
+    parser.add_argument("algorithm", type=str,
+                        help=""""Supported algorithm types:\n
+                         simulated_annealing_votes sav\n
+                         simulated_annealing_matrix sam\n
+                         genetic_algorithm_votes genv\n
+                         genetic_algorithm_matrix genm""")
+    parser.add_argument("--max_temperature", "--t", type=float, default=None)
+    parser.add_argument("--alpha", "--a", type=float, default=None)
+    parser.add_argument("--max_iterations", "--i", type=int, default=None)
+    parser.add_argument("--num_voters", "--v", type=int, default=None)
+    parser.add_argument("--changing_votes", "--c", type=int, default=None)
+    parser.add_argument("--neighbor_strategy", "--n", type=str, default=None)
+    parser.add_argument("--cooling_schedule", "--s", type=str, default=None)
+    parser.add_argument("--neighbor_weight", "--w", type=float, default=None)
+    parser.add_argument("--population_size", "--p", type=int, default=None)
+
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    run(
-        name='xd',
-        algorithm='genm',
-        reps=5,
-        population_size=4,
-        num_voters=5,
-        max_generations=3
-    )
+    arg_dict = parse_arguments()
+    print(arg_dict)
+    run(arg_dict)
